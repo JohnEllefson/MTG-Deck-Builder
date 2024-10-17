61bd58bae32102c791424fd57d6c9b0111f9a0da
John Ellefson <johnellefson@byui.edu>
2024-10-16 21:08:21 -0500

    Added a main page and tweaked the styling.

---
+++ src/js/main.js
@@ -0,0 +1,7 @@
+import { loadHeaderFooter } from "./utils.mjs";
+
+loadHeaderFooter().then(() => {
+  document.querySelector(".icon-cart").innerHTML =
+    localStorage.getItem("so-cart-quantity") || 0;
+  searchItem();
+});
---
+++ src/js/utils.mjs
@@ -0,0 +1,63 @@
+// wrapper for querySelector...returns matching element
+export function qs(selector, parent = document) {
+  return parent.querySelector(selector);
+}
+
+// retrieve data from localstorage
+export function getLocalStorage(key) {
+  return JSON.parse(localStorage.getItem(key));
+}
+
+// save data to local storage
+export function setLocalStorage(key, data) {
+  localStorage.setItem(key, JSON.stringify(data));
+}
+
+// set a listener for both touchend and click
+export function setClick(selector, callback) {
+  qs(selector).addEventListener("touchend", (event) => {
+    event.preventDefault();
+    callback();
+  });
+  qs(selector).addEventListener("click", callback);
+}
+
+// extract the query string from the URL
+export function getParams(params) {
+  const queryString = window.location.search;
+  const urlParams = new URLSearchParams(queryString);
+  const product = urlParams.get(params);
+  return product;
+}
+
+export function renderListWithTemplate(templateFn, parentElement, list, position = "afterbegin", clear = false) {
+  const htmlStrings = list.map(templateFn);
+  if (clear) {
+    parentElement.innerHTML = "";
+  }
+  parentElement.insertAdjacentHTML(position, htmlStrings.join(""));
+}
+
+export function renderWithTemplate(template, parent, data, callback) {
+  parent.insertAdjacentHTML("afterbegin", template);
+  if (callback) {
+    callback(data);
+  }
+}
+
+export async function loadTemplate(path) {
+  const response = await fetch(path);
+  const html = await response.text();
+  const template = document.createElement('template');
+  template.innerHTML = html;
+  return template;
+}
+
+export async function loadHeaderFooter() {
+  const headerContent = await loadTemplate("/partials/header.html");
+  const footerContent = await loadTemplate("/partials/footer.html");
+  const headerElement = document.querySelector("#dynamic-header");
+  const footerElement = document.querySelector("#dynamic-footer");
+  renderWithTemplate(headerContent.innerHTML, headerElement);
+  renderWithTemplate(footerContent.innerHTML, footerElement);
+}
